c35844dac61c3db42a825fa054a64f595eb4d3b2
Jyotiranjan Mohanty <jyotimohanty289@gmail.com>
2026-07-14 15:41:56 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,9 +49,11 @@ for (let i = 0; i < testimonialsItem.length; i++) {
 
 }
 
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
+// add click event to modal close button (guarded: testimonials modal markup may not be present)
+if (modalCloseBtn && overlay) {
+  modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+  overlay.addEventListener("click", testimonialsModalFunc);
+}
 
 
 
